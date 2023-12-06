8bdad1c7108a23b3c601b2f177d728b0a10dec9d
leotxsp <leotxsp@gmail.com>
2023-12-06 18:29:24 -0300

    Adicionado o controle de estoque

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 
-app = Flask(__name__)
 
+
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SECRET_KEY'] = 'UEUEHUEHEUHEUH'
+
 
 db = SQLAlchemy(app)
 
@@ -33,24 +36,35 @@ def index():
 
 @app.route('/search', methods=['POST', 'GET'])
 def search():
-    coluna_pesquisa = request.form['Colunas']
-    texto_pesquisa = request.form['TextoProcurar']
-    print(coluna_pesquisa,texto_pesquisa)
+    coluna_pesquisa = request.form.get('Colunas')
+    texto_pesquisa = request.form.get('TextoProcurar')
+
     if coluna_pesquisa == 'DESCRICAO':
-            products = produtos.query.filter(produtos.DESCRICAO.like(f"%{texto_pesquisa}%")).all()
-
-            return render_template('retorno.html', products=products)
+        products = produtos.query.filter(produtos.DESCRICAO.like(f"%{texto_pesquisa}%")).all()
     elif coluna_pesquisa == 'COD':
-            products = produtos.query.filter(produtos.CODIGO.like(f"%{texto_pesquisa}%")).all()
+        products = produtos.query.filter(produtos.CODIGO.like(f"%{texto_pesquisa}%")).all()
+    else:
+        products = []
 
-            return render_template('retorno.html', products=products)
-    
-    print(coluna_pesquisa,texto_pesquisa)
-
-    
-        
-   
     return render_template('retorno.html', products=products)
+
+
+
+@app.route('/update_stock', methods=['POST'])
+def update_stock():
+    product_id = request.form.get('product_id')
+    quantity = int(request.form.get('quantity'))
+
+    product = produtos.query.get(product_id)
+    if product:
+        product.QUANTIDADE += quantity
+        db.session.commit()
+        flash(f'Estoque atualizado para {product.QUANTIDADE} unidades.', 'success')
+    else:
+        flash('Produto não encontrado', 'error')
+
+    return redirect(url_for('search'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
